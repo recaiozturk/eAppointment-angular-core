d1d1ac5a0ea271479162c6c596aa7292137b3d2c
recaiozturk <recaiozturk54@gmail.com>
2025-02-03 15:46:52 +0300

    added appuser,patient

---
+++ eAppointmentServer/eAppointmentServer.Domain/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace eAppointmentServer.Domain.Entities
+{
+    public sealed class AppUser
+    {
+        public string Firstname { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => string.Join("", Firstname, LastName);
+    }
+}
---
+++ eAppointmentServer/eAppointmentServer.Domain/Entities/Patient.cs
@@ -0,0 +1,18 @@
+﻿namespace eAppointmentServer.Domain.Entities
+{
+    public sealed class Patient
+    {
+        public Patient()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string IdentityNumber { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Towtn { get; set; } = string.Empty;
+        public string FullAdress { get; set; } = string.Empty;
+    }
+}
